e48f4e7b96e6922736912aa64d1f7e49d508b44a
avhi sharma <avhi5949@gmail.com>
2025-06-23 15:57:14 +0530

    Fix:some login bugs

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -72,7 +72,7 @@ const Login = () => {
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100 dark:bg-gray-900 transition-colors duration-300 px-4">
       <div className="w-full max-w-md bg-white dark:bg-gray-800 text-gray-800 dark:text-white shadow-lg rounded-xl p-8 border border-gray-200 dark:border-gray-700">
-        <h1 className="text-3xl font-bold text-center mb-6">Login to FadeMart</h1>
+        <h1 className="text-3xl font-bold text-center mb-6">Login to Fade</h1>
 
         <form onSubmit={handleLogin} className="space-y-4">
           <input
