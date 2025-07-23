aab01efad0b3f0331909d5a9d15f7891da03e4a1
avhi sharma <avhi5949@gmail.com>
2025-07-23 20:14:36 +0530

    contact page updated

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -142,14 +142,15 @@ const Contact = () => {
             <SocialIcon href="https://www.facebook.com/profile.php?id=61577757806514" Icon={FaFacebookF} color="blue" name="Facebook" />
             <SocialIcon href="https://www.youtube.com/@FadeNetwork013" Icon={FaYoutube} color="red" name="YouTube" />
           </div>
-{/* 
-          Contact Owner Info
+
           <div className="mt-6 text-sm text-gray-600 dark:text-gray-400">
+          Contact Owner Info
             <p><strong>Owner:</strong> Sumit Kumar</p>
-            <p><strong>Email:</strong> <a href="mailto:support@fade013.store" className="text-blue-600 dark:text-blue-400">support@fade013.store</a></p>
-            <p><strong>Phone:</strong> <a href="tel:+9198789785951" className="text-blue-600 dark:text-blue-400">+91 87897 85951</a></p>
+            {/* <p><strong>Owner Email:</strong> <a href="mailto:fade@mail.io" className="text-blue-600 dark:text-blue-400">fade@mail.io</a></p> */}
+            <p><strong>Support Email:</strong> <a href="mailto:fade013@mail.io" className="text-blue-600 dark:text-blue-400">fade013@mail.io</a></p>
+            {/* <p><strong>Phone:</strong> <a href="tel:+9198789785951" className="text-blue-600 dark:text-blue-400">+91 87897 85951</a></p> */}
             <p><strong>Address:</strong> 028 Raj Colony, near ITI, Sunam, Punjab, India</p>
-          </div> */}
+          </div>
 
 
           <p className="text-gray-500 dark:text-gray-400 mt-4 text-sm">© 2025 Fade. All rights reserved.</p>
