cb34ed66a16e4258ec76101ab259d06ff3ad819c
avhi96 <avhisharma500@gmail.com>
2025-09-21 16:45:38 +0530

    database changed

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCrEnwkkEX0pFKY5UJVmkCU1L2OEsIECrk",
-  authDomain: "fademart-32de5.firebaseapp.com",
-  projectId: "fademart-32de5",
-  storageBucket: "fademart-32de5.firebasestorage.app",
-  messagingSenderId: "755726899118",
-  appId: "1:755726899118:web:b49445f6bd31953f5e1a3f",
-  measurementId: "G-N7S58YEDTH"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 
